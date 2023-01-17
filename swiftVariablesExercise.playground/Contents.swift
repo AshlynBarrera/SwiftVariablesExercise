2f36e13bbb7cba64defda76089cc549af0db821c
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-01-17 08:28:50 -0600

    Initial Commit

---
+++ swiftVariablesExercise.playground/Contents.swift
@@ -0,0 +1,29 @@
+
+//1.
+print("Ashlyn Barrera")
+
+//2 Int,Float,Double,Strings,Bool,Character
+
+//3 The LET variable type is constant.
+
+//4
+var person: String = "Chris"
+print(person)
+//5
+person = "Robert"
+print(person)
+
+
+//Bonus
+//6
+var number: Int = 3
+print(number)
+//7
+number = number + 2
+print(number)
+//8
+number = number + 5
+print(number)
+//9
+var nums: Array = [1,8,9]
+print(nums)
